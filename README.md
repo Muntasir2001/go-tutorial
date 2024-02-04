c00db3bfb09b197ea511abf8eec3f9ba33e490bf
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-02-04 09:21:07 +0000

    learned name return, multiple return values, how to ignore a variable if not being used (all func related)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Golang tutorial
+
+Tutorial followed: https://youtu.be/un6ZyFkqFKo?si=LL_7FMN4h1N7UTTC
--- main.go
+++ main.go
@@ -40,4 +40,57 @@ func main() {
 	} else {
 		fmt.Println("you are shorter")
 	}
+	
+	// conditional smaller version
+	/*
+		if INITIAL_STATEMENT; CONDITION {
+
+		}
+	*/
+	if length := 12; length < 1 {
+		fmt.Println("Number is invalid")
+	}
+
+	// functions
+	fmt.Println(concat("Hello", "World"))
+
+	// variables in Go variables are passed by value, not by reference
+	// eg:
+	sendsSoFar := 430
+	const sendsToAdd = 25
+
+	// to update sendsSoFar, sendsSoFar needs to be updated/reassigned to be updated
+	sendsSoFar = incrementSends(430, 25)
+	fmt.Println("you've sent", sendsSoFar, "messages")
+
+	// ignore a return value/variable (since golang doesn't allow unused variables)
+	firstName, _ := getNames()
+	fmt.Println(firstName)
+
+	// named return values
+	x, y := getCords()
+	fmt.Println(x, y)
+}
+
+// functions
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+func incrementSends(sendsSoFar, sendsToAdd int) int {
+	sendsSoFar = sendsSoFar + sendsToAdd
+	return sendsSoFar
+}
+
+// multiple return value
+func getNames() (string, string) {
+	return "John", "Doe"
+}
+
+// named return values
+func getCords() (x int, y int) {
+	x = 12
+	y = 1222
+	
+	return
 }
